7b0ba0bcd454c42b3a4d7e65b14cbb925c029850
mawglih <mawglih@gmail.com>
2023-05-18 17:28:40 -0400

    LC1 added

--- exercises_1-50/LC1_Two_Sum/index.js
+++ exercises_1-50/LC1_Two_Sum/index.js
@@ -7,6 +7,23 @@ You can return the answer in any order. */
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function(nums, target) {
-    
+var twoSum = function(nums, target) { //use hashMap tp splve in O(n)
+    let hashMap = new Map();
+    let result = []; //array of indices
+    for (let i = 0; i < nums.length; i++) {
+        let compensator = target - nums[i];
+        if(hashMap.has(compensator)) {
+            result.push(i, hashMap.get(compensator));
+            return result;
+        } else {
+            hashMap.set(nums[i], i);
+        }
+    }
+    return -1;
 }
+
+const arr = [2,7,11,15];
+let target = 9;
+console.log(twoSum(arr, target));
+// Runtime 65ms
+// Memory 43.7 MB
