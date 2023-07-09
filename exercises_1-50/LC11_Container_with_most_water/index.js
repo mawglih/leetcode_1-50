aa364a9eb9f183f05cdbbd400e41677fecba0981
mawglih <mawglih@gmail.com>
2023-07-09 14:09:51 -0400

    LC11-algo

--- exercises_1-50/LC11_Container_with_most_water/index.js
+++ exercises_1-50/LC11_Container_with_most_water/index.js
@@ -1,10 +1,4 @@
-/**You are given an integer array height of length n. There are n vertical lines drawn such that the two endpoints of the ith line are (i, 0) and (i, height[i]).
-
-Find two lines that together with the x-axis form a container, such that the container contains the most water.
-
-Return the maximum amount of water a container can store.
-
-Notice that you may not slant the container. */
+/**You are given an integer array height of length n. There are n vertical lines drawn such that the two endpoints of the ith line are (i, 0) and (i, height[i]). Find two lines that together with the x-axis form a container, such that the container contains the most water. Return the maximum amount of water a container can store. Notice that you may not slant the container. */
 
 /**
  * @param {number[]} height
@@ -32,3 +26,14 @@ console.log(maxArea(arr));
 
 // runtime 82 ms
 // memory 50.1 mb
+
+//same but concise
+
+var maxArea = function(height) {
+    let maxArea = 0, l = 0, r = height.length - 1;
+    while(l < r){
+        maxArea = Math.max(maxArea, Math.min(height[l],height[r]) * (r - l))
+        height[l] < height[r] ? l++ : r--       
+    }
+    return maxArea;
+};
